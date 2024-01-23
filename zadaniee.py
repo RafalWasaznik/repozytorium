fbc27ec58be0a7acd388b8b3391549c52f1449af
rafal <rafalwasaznik@onet.pl>
2024-01-23 14:39:54 +0100

    Initial Commit

--- zadaniee.py
+++ zadaniee.py
@@ -1,13 +1,13 @@
-# Pobieramy od użytkownika wysokość diamentu 
+# Pobieramy od użytkownika wysokość diamentu
 height = int(input("Podaj wysokość diamentu (najlepiej liczba nieparzysta): "))
 
-# Górna część diamentu 
+# Górna część diamentu
 for i in range(1, height, 2):
     print(" " * ((height - i) // 2) + "*" * i + " " * ((height - i) // 2))
 
-# Środkowa część diamentu 
+# Środkowa część diamentu
 print("*" * height)
 
-# Dolna część diamentu 
+# Dolna część diamentu
 for i in range(height - 2, 0, -2):
-    print(" " * ((height - i) // 2) + "*" * i + " " * ((height - i) // 2)) 
+    print(" " * ((height - i) // 2) + "*" * i + " " * ((height - i) // 2))
